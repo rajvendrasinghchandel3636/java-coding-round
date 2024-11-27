94ad72333a04838d328d0a01bcb8d4f210606815
Rajvendra singh chandel <149138849+rajvendrasinghchandel3636@users.noreply.github.com>
2024-11-27 19:23:04 +0530

    Add files via upload

---
+++ reversearray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class reversearray {
+
+
+public static void main(String[] args){
+    int[] arr = {1,2,3,4,5};
+    System.out.println("array" + Arrays.toString(arr));
+
+
+    int n = arr.length;
+    
+    for (int i=0; i<n/2; i++){
+        int temp = arr[i];
+        arr[i] = arr[n - i -1];
+
+        arr[n - i -1] = temp;
+    }
+    System.out.println("reversed" + Arrays.toString(arr));
+}
+}
